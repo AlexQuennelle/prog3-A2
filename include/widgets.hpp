5678bd956dc7790c98b9fc61978d8ad058225d4a
Alex Quennelle <BluePhoenixCG@gmail.com>
2025-09-17 15:50:28 -0500

    Start work

---
+++ include/widgets.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <raylib.h>
+
+namespace wig
+{
+
+class iWidget
+{
+	protected:
+	Vector2 position;
+};
+
+class Button : public iWidget
+{};
+
+class WidgetProgram
+{
+	public:
+	private:
+};
+
+} //namespace wig
